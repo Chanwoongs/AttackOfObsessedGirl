742ea80670f72fd29c8186bea2d46e7665d6ea7a
Chanwoongs <tkfkdcksdnd@daum.net>
2023-02-10 10:27:32 +0900

    Added Structure that gives data of switching line and state

--- Assets/Scripts/Game/Dialog.cs
+++ Assets/Scripts/Game/Dialog.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct SwitchingData
+{
+    public int switchingLineNum;
+
+    public SpriteState leftState;
+    public SpriteState rightState;
+
+    public SwitchingData(int switchingLineNum = -1, SpriteState leftState = SpriteState.Max, SpriteState rightState = SpriteState.Max)
+    {
+        this.switchingLineNum = switchingLineNum;
+        this.leftState = leftState;
+        this.rightState = rightState;
+    }
+}
+
 [System.Serializable]
 public class Dialog 
 {
     [SerializeField] List<string> lines;
 
-    public List<string> Lines { get { return lines; } }
+    public List<SwitchingData> switchingDatas = new List<SwitchingData>();
+
+    public List<string> Lines { get => lines; }
+    public List<SwitchingData> SwitchingDatas { get => switchingDatas; } 
 }
