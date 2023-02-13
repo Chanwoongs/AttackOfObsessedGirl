82d11f18aa7d7158a559e20fd075d8781471c1c2
Chanwoongs <tkfkdcksdnd@daum.net>
2023-02-13 19:44:47 +0900

    TabTab Minigame

---
+++ Assets/Scripts/QTEs/TabTabEvent.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class TabTabEvent : MonoBehaviour, IMinigame
+{
+    [SerializeField] Slider progressBar;
+    public bool IsSucceed { get; set; }
+    public bool IsPlaying { get; set; }
+
+    public event Action OnSuccess;
+    public event Action OnFailure;
+
+    private void Start()
+    {
+        IsSucceed = false;
+    }
+
+    public IEnumerator StartMinigame()
+    {
+        progressBar.value = 0;
+        IsPlaying = true;
+        IsSucceed = false;
+
+        yield return null;
+    }
+
+    public IEnumerator Succeed()
+    {
+        // 성공 효과
+        OnSuccess?.Invoke();
+        IsPlaying = false;
+
+        Destroy(this.gameObject);
+        yield return null;
+    }
+
+    public IEnumerator Failed()
+    {
+        // 실패 효과
+        OnFailure?.Invoke();
+        IsPlaying = false;
+
+        Destroy(this);
+        yield return null;
+    }
+
+    private void Update()
+    {
+         if (IsSucceed) { return; }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsPlaying)
+        {
+            progressBar.value += 50.0f;
+        }
+
+        if (progressBar.value > 99f)
+        {
+            IsSucceed = true;
+            StartCoroutine(Succeed());
+            return;
+        }
+
+        progressBar.value -= 1.0f * Time.deltaTime;
+    }
+}
